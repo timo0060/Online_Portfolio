16e6fc1a9eec2d0116a29f72a14ead91a8a66f03
Tim Radder <timo0060@gmail.com>
2014-04-09 13:37:36 -0400

    The Basics
    After creating a header, I noticed an vast emptiness. I was not happy.
    So I created the footer so that the page will have an ending, and I
    created a basic home page, so others may find where they are.

---
+++ footer.php
@@ -0,0 +1,7 @@
+	</body>
+
+	<footer>
+		<p>Copyright @ 2014<br>Timothy Radder</p>
+	</footer>
+	
+</html>
---
+++ home.php
@@ -0,0 +1,9 @@
+	<?php	
+		require_once('header.php');	
+	?>
+
+	This is the Home page!!!
+	
+	<?php
+		require_once('footer.php');
+	?>
