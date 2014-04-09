59fc61694e94839244bc90310aea0871dc58cd5d
Tim Radder <timo0060@gmail.com>
2014-04-09 13:30:59 -0400

    The new Beginning
    Now more have been added, a header is now present, just needs a body and
    the footer. More to come shortly

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Timothy Radder</title>
+	</head>
+	
+	<body>
+		<div id="header">
+			<div id="navBar">
+				<a href="home.php">HOME</a> ||  <a href="about.php">ABOUT</a>  ||  <a href="contact.php">Contact</a>
+			</div>
+		</div>
+	
